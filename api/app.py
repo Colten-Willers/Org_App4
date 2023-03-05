9edd9ee79f5673359986a7bb79901177aed3f204
Colten Willers <70586783+Colten-Willers@users.noreply.github.com>
2023-03-05 19:28:04 +0100

    Updated app.py.

--- api/app.py
+++ api/app.py
@@ -8,8 +8,6 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 app.config['SECRET_KEY'] = 'ultra_secret_key'
-# app.secret_key = 'ultra_secret_key'
-# app.config.update(SECRET_KEY = 'ultra_secret_key')
 
 app.config['SESSION_PERMANENT'] = True
 
